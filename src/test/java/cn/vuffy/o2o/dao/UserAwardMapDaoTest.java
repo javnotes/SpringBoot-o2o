1e54d98ad9ccb849e3723bdb56139aa6dd65ba59
sksvsong <liliansong_2019@163.com>
2021-07-22 17:33:20 +0800

    add UserAwardMapDao

---
+++ src/test/java/cn/vuffy/o2o/dao/UserAwardMapDaoTest.java
@@ -0,0 +1,15 @@
+package cn.vuffy.o2o.dao;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author vuffy
+ * @create 2021-07-22 17:06
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class UserAwardMapDaoTest {
+
+}
