98f5d487fe9658bfe5413c8c4d97a6162515f890
vuffy <monkeyd.vuffy@gmail.com>
2021-08-01 16:25:42 +0800

    .

--- src/main/java/cn/vuffy/o2o/O2oApplication.java
+++ src/main/java/cn/vuffy/o2o/O2oApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class O2oApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(O2oApplication.class, args);
     }
